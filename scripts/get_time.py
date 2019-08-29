70e50e4882ecca9c73ef418b03b6820184a46e2b
jinyanhuan <jinyanhuan@gmail.com>
2019-08-29 15:12:21 +0800

    fix the problem when reading csv

--- scripts/get_time.py
+++ scripts/get_time.py
@@ -1,13 +1,7 @@
-class SortGetTime(object):
+class GetTime(object):
     def __init__(self, data, date_column):
         self.data = data
         self.date_column = date_column
-
-    def sort(self):
-        self.data = self.data.sort_values(by=[self.date_column])
-        self.data = self.data.reset_index()
-        self.data = self.data.drop(columns="index")
-        return self.data
 
     def get_time(self):
         first_day = self.data[self.date_column][0]
--- scripts/main.py
+++ scripts/main.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import load_data
 from generator import GenerateDate
-from sort_get_time import SortGetTime
+from get_time import GetTime
 
 
 def merge(date_list, name_list):
@@ -33,23 +33,22 @@ def generate_open(df, target_column, flag="y"):
 def main():
 
     # 1.导入文件，确定日期列，填充列，目标列
-    data_name = input("请输入要转化的文件全名:(例如:test.csv)\n")
+    data_name = input("请输入要转化的文件全名:(例如:all_in_one.csv)\n")
     date_column = input("请输入日期所在的列名:\n")
     target_column = input("请输入预测目标所在的列名:\n")
     type_column = input("请输入需要填充的item(目前仅支持一个):\n")
     loader = load_data.LoadData()
     origin_data = loader.load_data(data_name)
 
-    # 2.对日期列按日期排序,然后根据时间戳范围，生成完整的时间戳
-    sorter = SortGetTime(origin_data, date_column)
-    sorter.sort()
-    sorted_data, first_day, last_day = sorter.get_time()
-    sorted_data[date_column] = pd.to_datetime(sorted_data[date_column])
+    # 2.根据时间戳范围，生成完整的时间戳
+    timer = GetTime(origin_data, date_column)
+    data, first_day, last_day = timer.get_time()
+    data[date_column] = pd.to_datetime(data[date_column])
     generate_date = GenerateDate(first_day, last_day)
     dates = generate_date.generate()
 
     # 3.将时间戳和名称合并为DataFrame_1
-    name_list = list(set(sorted_data[type_column]))
+    name_list = list(set(data[type_column]))
     if str(name_list[0]) == "nan":
         name_list.pop(0)
     else:
@@ -58,7 +57,7 @@ def main():
     df_merge = pd.DataFrame(merge_list, columns=[date_column, type_column])
 
     # 4.将DataFrame_1和原来的DataFrame外连接合并
-    df_total = pd.merge(df_merge, sorted_data, how="outer", on=[date_column, type_column])
+    df_total = pd.merge(df_merge, data, how="outer", on=[date_column, type_column])
 
     # 5.额外选项
     df_total[date_column] = pd.to_datetime(df_total[date_column])
@@ -73,8 +72,10 @@ def main():
     export_name_first = data_name.split(".")[0] + "_out"
     export_name_end = data_name.split(".")[-1]
     export_name = export_name_first + "." + export_name_end
-    df_total.to_excel(export_name, index_label="ID")
-
+    if export_name_end == "xlsx":
+        df_total.to_excel(export_name, index_label="ID")
+    elif export_name_end == "csv":
+        df_total.to_csv(export_name, index_label="ID")
 
 if __name__ == "__main__":
     main()
